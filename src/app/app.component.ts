1626bbce8ed246d94f60da65f18efba20affc4e5
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-07 10:18:04 +0200

    added @if statement and tested it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-user',
-  // template:`Hello Universe`,
-  template:`
-  Username : {{ username }}`,
-  styles:`
-  :host{
-    color:#a144eb;
-  }
-  `,
-  standalone: true,
-  imports: [RouterOutlet],
-  // templateUrl: './app.component.html',
-  // styleUrl: './app.component.css'
-})
-export class UserComponent {
-  username = `Nathan`;
-}
+// @Component({
+//   selector: 'app-user',
+//   // template:`Hello Universe`,
+//   template:`
+//   Username : {{ username }}`,
+//   styles:`
+//   :host{
+//     color:#a144eb;
+//   }
+//   `,
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   // templateUrl: './app.component.html',
+//   // styleUrl: './app.component.css'
+// })
+// export class UserComponent {
+//   username = `Nathan`;
+// }
 
 
 @Component({
   selector: 'app-root',
   // template:`Hello Universe`,
   template:`
-  <section>
-  <app-user/>
-  </section>
+ 
+  @if (isServerRunning) {
+    <span>Yes, the server is running</span>
+    } @else {
+    <span>No, the server is not running</span>
+    }
+
   `,
   styles:`
   :host{
@@ -35,8 +39,10 @@ export class UserComponent {
   }
   `,
   standalone: true,
-  imports: [RouterOutlet,UserComponent],
+  imports: [RouterOutlet,],
   // templateUrl: './app.component.html',
   // styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  isServerRunning = true ;
+}
