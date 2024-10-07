45960e4f6c29f6192751136000acea5cf64bf72b
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-07 09:10:30 +0200

    prac 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,16 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  template:`Hello Universe`,
+  styles:`
+  :host{
+    color:#a144eb;
+  }
+  `,
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'oop';
