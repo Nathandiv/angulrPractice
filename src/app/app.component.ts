cf567b5dc58df735ace26290cd382f468caf10a5
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-07 10:43:42 +0200

    added an array for the objects to be inserted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,16 +21,38 @@ import { RouterOutlet } from '@angular/router';
 // }
 
 
+// @Component({
+//   selector: 'app-root',
+//   // template:`Hello Universe`,
+//   template:`
+ 
+//   @if (isServerRunning) {
+//     <span>Yes, the server is running</span>
+//     } @else {
+//     <span>No, the server is not running</span>
+//     }
+
+//   `,
+//   styles:`
+//   :host{
+//     color:#a144eb;
+//   }
+//   `,
+//   standalone: true,
+//   imports: [RouterOutlet,],
+//   // templateUrl: './app.component.html',
+//   // styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   isServerRunning = true ;
+// }
+
 @Component({
   selector: 'app-root',
   // template:`Hello Universe`,
   template:`
  
-  @if (isServerRunning) {
-    <span>Yes, the server is running</span>
-    } @else {
-    <span>No, the server is not running</span>
-    }
+
 
   `,
   styles:`
@@ -44,5 +66,7 @@ import { RouterOutlet } from '@angular/router';
   // styleUrl: './app.component.css'
 })
 export class AppComponent {
-  isServerRunning = true ;
+  users = [
+   
+  ];
 }
