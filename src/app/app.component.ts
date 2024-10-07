22a975efa06d4136ecb8df14b7a86ec1c4e299c8
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-07 10:45:38 +0200

    added the objects in the array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -67,6 +67,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   users = [
-   
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'},
   ];
 }
